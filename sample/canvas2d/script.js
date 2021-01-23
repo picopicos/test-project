fc53043333ed6fda5596046be2fd6cc346dcd936
picopicos <picopicoico@gmail.com>
2021-01-23 10:33:21 +0900

    Update Canvas2D loading.

--- sample/canvas2d/script.js
+++ sample/canvas2d/script.js
@@ -1,5 +1,26 @@
-let canvas = document.body.querySelector('#mainCanvas');
+(() => {[]
+let canvas = null;
+let ctx = null;
 
-let context = canvas.getContext('2d');
+// ページのロード完了時に発火するloadイベント]
+window.addEventListener('load', () => {
+    initialize();
+    render();
+}, false);
 
-context.fillRect(0, 0, 100, 100);
+// 初期化関数の定義
+function initialize(){
+    canvas = document.body.querySelector('#mainCanvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx = canvas.getContext('2d');
+}
+
+// 描画関数の定義
+function render(){
+    ctx.fillStyle = '#000000';
+
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+})();
